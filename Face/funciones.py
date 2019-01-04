7f3115860cafc83c695e2b67ee340058f4a6396e
Jona <jon_ra@hotmail.es>
2019-01-03 22:02:03 -0500

    cambios

---
+++ Face/funciones.py
@@ -0,0 +1,75 @@
+import cv2
+
+def detectFaceOpenCVHaar(faceCascade, frame, inHeight=300, inWidth=0):
+    frameOpenCVHaar = frame.copy()
+    frameHeight = frameOpenCVHaar.shape[0]
+    frameWidth = frameOpenCVHaar.shape[1]
+    if not inWidth:
+        inWidth = int((frameWidth / frameHeight) * inHeight)
+
+    scaleHeight = frameHeight / inHeight
+    scaleWidth = frameWidth / inWidth
+
+    frameOpenCVHaarSmall = cv2.resize(frameOpenCVHaar, (inWidth, inHeight))
+    frameGray = cv2.cvtColor(frameOpenCVHaarSmall, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(frameGray)
+    bboxes = []
+    for (x, y, w, h) in faces:
+        x1 = x
+        y1 = y
+        x2 = x + w
+        y2 = y + h
+        cvRect = [int(x1 * scaleWidth), int(y1 * scaleHeight),
+                  int(x2 * scaleWidth), int(y2 * scaleHeight)]
+        bboxes.append(cvRect)
+        cv2.rectangle(frameOpenCVHaar, (cvRect[0], cvRect[1]), (cvRect[2], cvRect[3]), (0, 255, 0),
+                      int(round(frameHeight / 150)), 4)
+    return frameOpenCVHaar, bboxes
+
+
+def detectFaceOpenCVDnn(net, frame, conf_threshold):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], False, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+
+def detectFaceDlibHog(detector, frame, inHeight=300, inWidth=0):
+
+    frameDlibHog = frame.copy()
+    frameHeight = frameDlibHog.shape[0]
+    frameWidth = frameDlibHog.shape[1]
+    if not inWidth:
+        inWidth = int((frameWidth / frameHeight)*inHeight)
+
+    scaleHeight = frameHeight / inHeight
+    scaleWidth = frameWidth / inWidth
+
+    frameDlibHogSmall = cv2.resize(frameDlibHog, (inWidth, inHeight))
+
+    frameDlibHogSmall = cv2.cvtColor(frameDlibHogSmall, cv2.COLOR_BGR2RGB)
+    faceRects = detector(frameDlibHogSmall, 0)
+    #print(frameWidth, frameHeight, inWidth, inHeight)
+    bboxes = []
+    for faceRect in faceRects:
+
+        cvRect = [int(faceRect.left()*scaleWidth), int(faceRect.top()*scaleHeight),
+                  int(faceRect.right()*scaleWidth), int(faceRect.bottom()*scaleHeight) ]
+        bboxes.append(cvRect)
+        cv2.rectangle(frameDlibHog, (cvRect[0], cvRect[1]), (cvRect[2], cvRect[3]), (0, 255, 0), int(round(frameHeight/150)), 4)
+    return frameDlibHog, bboxes
--- Face/prueba1.py
+++ Face/prueba1.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 from imutils import paths
-import face_recognition
+from funciones import detectFaceOpenCVHaar, detectFaceOpenCVDnn, detectFaceDlibHog
+import face_recognition  #paquete de python
 import argparse
 import pickle
 import cv2
@@ -42,80 +43,6 @@ conf_threshold = 0.7
 # DLIB HoG
 hogFaceDetector = dlib.get_frontal_face_detector()
 
-def detectFaceOpenCVHaar(faceCascade, frame, inHeight=300, inWidth=0):
-    frameOpenCVHaar = frame.copy()
-    frameHeight = frameOpenCVHaar.shape[0]
-    frameWidth = frameOpenCVHaar.shape[1]
-    if not inWidth:
-        inWidth = int((frameWidth / frameHeight) * inHeight)
-
-    scaleHeight = frameHeight / inHeight
-    scaleWidth = frameWidth / inWidth
-
-    frameOpenCVHaarSmall = cv2.resize(frameOpenCVHaar, (inWidth, inHeight))
-    frameGray = cv2.cvtColor(frameOpenCVHaarSmall, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(frameGray)
-    bboxes = []
-    for (x, y, w, h) in faces:
-        x1 = x
-        y1 = y
-        x2 = x + w
-        y2 = y + h
-        cvRect = [int(x1 * scaleWidth), int(y1 * scaleHeight),
-                  int(x2 * scaleWidth), int(y2 * scaleHeight)]
-        bboxes.append(cvRect)
-        cv2.rectangle(frameOpenCVHaar, (cvRect[0], cvRect[1]), (cvRect[2], cvRect[3]), (0, 255, 0),
-                      int(round(frameHeight / 150)), 4)
-    return frameOpenCVHaar, bboxes
-
-
-
-def detectFaceOpenCVDnn(net, frame):
-    frameOpencvDnn = frame.copy()
-    frameHeight = frameOpencvDnn.shape[0]
-    frameWidth = frameOpencvDnn.shape[1]
-    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], False, False)
-
-    net.setInput(blob)
-    detections = net.forward()
-    bboxes = []
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > conf_threshold:
-            x1 = int(detections[0, 0, i, 3] * frameWidth)
-            y1 = int(detections[0, 0, i, 4] * frameHeight)
-            x2 = int(detections[0, 0, i, 5] * frameWidth)
-            y2 = int(detections[0, 0, i, 6] * frameHeight)
-            bboxes.append([x1, y1, x2, y2])
-            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
-    return frameOpencvDnn, bboxes
-
-
-def detectFaceDlibHog(detector, frame, inHeight=300, inWidth=0):
-
-    frameDlibHog = frame.copy()
-    frameHeight = frameDlibHog.shape[0]
-    frameWidth = frameDlibHog.shape[1]
-    if not inWidth:
-        inWidth = int((frameWidth / frameHeight)*inHeight)
-
-    scaleHeight = frameHeight / inHeight
-    scaleWidth = frameWidth / inWidth
-
-    frameDlibHogSmall = cv2.resize(frameDlibHog, (inWidth, inHeight))
-
-    frameDlibHogSmall = cv2.cvtColor(frameDlibHogSmall, cv2.COLOR_BGR2RGB)
-    faceRects = detector(frameDlibHogSmall, 0)
-    #print(frameWidth, frameHeight, inWidth, inHeight)
-    bboxes = []
-    for faceRect in faceRects:
-
-        cvRect = [int(faceRect.left()*scaleWidth), int(faceRect.top()*scaleHeight),
-                  int(faceRect.right()*scaleWidth), int(faceRect.bottom()*scaleHeight) ]
-        bboxes.append(cvRect)
-        cv2.rectangle(frameDlibHog, (cvRect[0], cvRect[1]), (cvRect[2], cvRect[3]), (0, 255, 0), int(round(frameHeight/150)), 4)
-    return frameDlibHog, bboxes
 
 
 source = 0
@@ -155,7 +82,7 @@ while(cap.isOpened()):
     cv2.putText(outOpencvHaar, label, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3, cv2.LINE_AA)
 
     t = time.time()
-    outOpencvDnn, bboxes = detectFaceOpenCVDnn(net,frame)
+    outOpencvDnn, bboxes = detectFaceOpenCVDnn(net,frame, conf_threshold)
     tt_opencvDnn += time.time() - t
     fpsOpencvDnn = frame_count / tt_opencvDnn
     label = "OpenCV DNN ; FPS : {:.2f}".format(fpsOpencvDnn)
